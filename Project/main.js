5cb5a12039896116daad084f2162aeb6d4b720a6
adamgorman <adgorman@umich.edu>
2014-04-01 00:53:53 -0400

    Logout Popup
    - Logout now makes a popup
    - Still need to pull the header and slider out of the main.html.  Ideas?
    - Created directories for some of the screens

--- Project/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Adam on 3/27/14.
- */
-
-//$(document).ready(function() {
-  //  $(".header-child").load('header.html');
-//});
---
+++ Project/main/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Adam on 3/27/14.
+ */
+
+
