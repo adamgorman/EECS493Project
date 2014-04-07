fac7e004cfae45ddd0d6d7610d10549c1e1a9273
Carla Kaspar <cskas@umich.edu>
2014-04-07 17:05:13 -0400

    fb share that doesn't work :(

--- Project/achievements/achievements.js
+++ Project/achievements/achievements.js
@@ -15,3 +15,29 @@ $( "#popupPanel" ).on({
     }
 });
 
+
+$("li").click(function () {
+    FB.init({appId: 496828853760637,
+        status: true,
+        cookie:true
+    });
+    FB.ui(
+        {
+            method: "stream.publish",
+            display: "iframe",
+            user_message_prompt: "Publish this",
+            attachment: {
+                name: "Joe Schmoe's achievement on Workout Carrot",
+                description: "Joe Schmoe worked out 6 times this week!"
+//                        media: [("src", 'dumbell.jpg')]
+            }
+        },
+        function (response) {
+            if (response && response.post_id) {
+                alert('Post was published.');
+            } else {
+                alert('Post was not published.');
+            }
+        }
+    );
+});
