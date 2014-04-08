02c2ff9592ff1f3c111f543645c78d88c2cefa20
Carla Kaspar <cskas@umich.edu>
2014-04-07 22:43:01 -0400

    dialog

--- Project/achievements/achievements.js
+++ Project/achievements/achievements.js
@@ -7,37 +7,30 @@ $('#mydiv').share({
     networks: ['facebook','twitter']
 });
 
-$( "#popupPanel" ).on({
-    popupbeforeposition: function() {
-        var h = $( window ).height();
-
-        $( "#popupPanel" ).css( "height", h );
-    }
-});
 
 
-$("li").click(function () {
-    FB.init({appId: 496828853760637,
-        status: true,
-        cookie:true
-    });
-    FB.ui(
-        {
-            method: "stream.publish",
-            display: "iframe",
-            user_message_prompt: "Publish this",
-            attachment: {
-                name: "Joe Schmoe's achievement on Workout Carrot",
-                description: "Joe Schmoe worked out 6 times this week!"
-//                        media: [("src", 'dumbell.jpg')]
-            }
-        },
-        function (response) {
-            if (response && response.post_id) {
-                alert('Post was published.');
-            } else {
-                alert('Post was not published.');
-            }
-        }
-    );
-});
+//$("li").click(function () {
+//    FB.init({appId: 496828853760637,
+//        status: true,
+//        cookie:true
+//    });
+//    FB.ui(
+//        {
+//            method: "stream.publish",
+//            display: "iframe",
+//            user_message_prompt: "Publish this",
+//            attachment: {
+//                name: "Joe Schmoe's achievement on Workout Carrot",
+//                description: "Joe Schmoe worked out 6 times this week!"
+////                        media: [("src", 'dumbell.jpg')]
+//            }
+//        },
+//        function (response) {
+//            if (response && response.post_id) {
+//                alert('Post was published.');
+//            } else {
+//                alert('Post was not published.');
+//            }
+//        }
+//    );
+//});
