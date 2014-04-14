20a6b7941fa108b0ab8aa8f9203f63a83b3adb8f
Carla Kaspar <cskas@umich.edu>
2014-04-14 18:13:59 -0400

    achievements and news feed

--- Project/achievements/achievements.js
+++ Project/achievements/achievements.js
@@ -2,23 +2,20 @@
  * Created by Carla on 4/5/2014.
  */
 
-$('#mydiv').share({
-    networks: ['facebook','twitter']
-});
 
 var achv_array=["one", "two", "three", "four", "five", "six","seven","eight","nine","ten"];
 for (var i = 0; i < achv_array.length; ++i) { achv_array[i] = false; }
 
 var achv0 = ('<table><tr><td><img src="dumbbell.jpg"></td><td><h2 class ="adjust_indent">Beginner Bunny Achieved!</h2>' +
-    '<p class ="adjust_indent">You worked out 5 times in 1 week</p></td></tr></table>');
+    '<p class ="adjust_indent">You worked out 5 days in a row</p></td></tr></table>');
 var achv1=('<table><tr><td><img src="dumbbell.jpg"></td><td><h2 class ="adjust_indent">Healthy Hare Achieved!</h2>'+
-    '<p class ="adjust_indent">You worked out 6 times in 1 week</p></td></tr></table>');
+    '<p class ="adjust_indent">You worked out 6 days in a row</p></td></tr></table>');
 var achv2=('<table><tr><td><img src="dumbbell.jpg"></td><td><h2 class ="adjust_indent">Radical Rabbit Achieved!</h2>'+
-    '<p class ="adjust_indent">You worked out 7 times in 1 week</p></td></tr></table>');
+    '<p class ="adjust_indent">You worked out 7 days in a row</p></td></tr></table>');
 var achv3=('<table><tr><td><img src="healthy.jpg"></td><td><h2 class ="adjust_indent">Baby Carrot Achieved!</h2>'+
-    '<p class ="adjust_indent">Under 2000 calories 3 days in 1 week</p></td></tr></table>');
+    '<p class ="adjust_indent">Under 2000 calories 3 days in a row</p></td></tr></table>');
 var achv4=('<table><tr><td><img src="healthy.jpg"></td><td><h2 class ="adjust_indent">Carrot Pro Achieved!</h2>'+
-    '<p class ="adjust_indent">Under 2000 calories 5 days in 1 week</p></td></tr></table>');
+    '<p class ="adjust_indent">Under 2000 calories 5 days in a row</p></td></tr></table>');
 var achv5=('<table><tr><td><img src="scale.jpg"></td><td><h2 class ="adjust_indent">Slim Carrot Achieved!</h2>'+
     '<p class ="adjust_indent">Lost 3 pounds from starting weight</p></td></tr></table>');
 var achv6=('<table><tr><td><img src="scale.jpg"></td><td><h2 class ="adjust_indent">Slender Carrot Achieved!</h2>'+
@@ -29,6 +26,11 @@ var achv8=('<table><tr><td><img src="checkmark.jpg"></td><td><h2 class ="adjust_
     '<p class ="adjust_indent">Logged in 10 days in a row</p></td></tr></table>');
 var achv9=('<table><tr><td><img src="checkmark.jpg"></td><td><h2 class ="adjust_indent">15 Carrot Log Achieved!</h2>'+
     '<p class ="adjust_indent">Logged in 15 days in a row</p></td></tr></table>');
+
+var workout_counter = 5;
+var under_2000 = 4;
+var weight_counter = -3;
+var login_counter = 10;
 Parse.initialize("PAjzQPglIFtzZyMJfDPe5Ozvfzr7Pz1ukpHoLXct", "zTU8emhQIg5xTKZrhfF5ulrKnXQv4M6ztT9kFi90");
 
 var username = "username";
@@ -40,15 +42,31 @@ var user = Parse.User.logIn(username, password, {
         // if login is successful
 //        alert("You have logged in");
         var aArray = user.get("achievementArray");
-        if(!aArray[0]) {
-            $("#a1").html(achv0);
-            $("#ws1").attr("href", "#share_achievement");
-        };
-        $("#ws1").click(function () {
-            $("#dup").html(achv0);
-//    e.preventDefault();
+        //    e.preventDefault();
+        if(workout_counter >= 5)
+            aArray[0]=1;
+        if(workout_counter >= 6)
+            aArray[1]=1;
+        if(workout_counter >= 7)
+            aArray[2]=1;
+        if(under_2000 >= 3)
+            aArray[3]=1;
+        if(under_2000 >= 5)
+            aArray[4]=1;
+        if(weight_counter <= -3)
+            aArray[5]=1;
+        if(weight_counter <= -5)
+            aArray[6]=1;
+        if(login_counter >= 5)
+            aArray[7]=1;
+        if(login_counter >= 10)
+            aArray[8]=1;
+        if(login_counter >= 15)
+            aArray[9]=1;
+
+        $("#fb_icon").click(function () {
             FB.init({
-//                appId : '496828853760637',
+                //appId : '496828853760637',
                 //apiKey: '95df48eb35db42b7a4dbca37b4e55bae'
                 appId: '1578778675680791',
                 apiKey: 'a092a4e59e6b899c2a2e38c4b8adbc96',
@@ -58,26 +76,33 @@ var user = Parse.User.logIn(username, password, {
                 link: 'http://workoutcarrot.parseapp.com/'
             });
             FB.ui(
-                {
-                    method: 'feed',
-                    name: 'This is the content of the "name" field.'
-                });
+            {
+                method: 'feed',
+                name: 'This is the content of the "name" field.'
+            });
         });
-        if(!aArray[1]){
+        if(aArray[0]) {
+            $("#a1").html(achv0);
+            $("#ws1").attr("href", "#share_achievement");
+        };
+        $("#ws1").click(function () {
+            $("#dup").html(achv0);
+        });
+        if(aArray[1]){
             $("#a2").html(achv1);
             $("#ws2").attr("href","#share_achievement");
         }
         $("#ws2").click(function () {
             $("#dup").html(achv1);
         });
-        if(!aArray[2]) {
+        if(aArray[2]) {
             $("#a3").html(achv2);
             $("#ws3").attr("href","#share_achievement");
         }
         $("#ws3").click(function () {
             $("#dup").html(achv2);
         });
-        if(!aArray[3]){
+        if(aArray[3]){
 
             $("#b1").html(achv3);
             $("#hs1").attr("href","#share_achievement");
@@ -86,7 +111,7 @@ var user = Parse.User.logIn(username, password, {
 
             $("#dup").html(achv3);
         });
-        if(!aArray[4]) {
+        if(aArray[4]) {
             $("#b2").html(achv4);
             $("#hs2").attr("href","#share_achievement");
         }
@@ -94,14 +119,14 @@ var user = Parse.User.logIn(username, password, {
             $("#dup").html(achv4);
         });
 
-        if(!aArray[5]){
+        if(aArray[5]){
             $("#c1").html(achv5);
             $("#ls1").attr("href","#share_achievement");
         }
         $("#ls1").click(function () {
             $("#dup").html(achv5);
         });
-        if(!aArray[6]) {
+        if(aArray[6]) {
             $("#c2").html(achv6);
             $("#ls2").attr("href","#share_achievement");
         }
@@ -109,21 +134,21 @@ var user = Parse.User.logIn(username, password, {
             $("#dup").html(achv6);
         });
 
-        if(!aArray[7]) {
+        if(aArray[7]) {
             $("#d1").html(achv7);
             $("#is1").attr("href","#share_achievement");
         }
         $("#is1").click(function () {
             $("#dup").html(achv7);
         });
-        if(!aArray[8]) {
+        if(aArray[8]) {
             $("#d2").html(achv8);
             $("#is2").attr("href","#share_achievement");
         }
         $("#is2").click(function () {
             $("#dup").html(achv8);
         });
-        if(!aArray[9]) {
+        if(aArray[9]) {
             $("#d3").html(achv9);
             $("#is3").attr("href","#share_achievement");
         }
@@ -137,35 +162,3 @@ var user = Parse.User.logIn(username, password, {
         alert("Error: " + error.code + " " + error.message);
     }
 });
-
-
-
-
-
-
-
-//$("li").click(function () {
-//    FB.init({appId: 496828853760637,
-//        status: true,
-//        cookie:true
-//    });
-//    FB.ui(
-//        {
-//            method: "stream.publish",
-//            display: "iframe",
-//            user_message_prompt: "Publish this",
-//            attachment: {
-//                name: "Joe Schmoe's achievement on Workout Carrot",
-//                description: "Joe Schmoe worked out 6 times this week!"
-////                        media: [("src", 'dumbell.jpg')]
-//            }
-//        },
-//        function (response) {
-//            if (response && response.post_id) {
-//                alert('Post was published.');
-//            } else {
-//                alert('Post was not published.');
-//            }
-//        }
-//    );
-//});
--- Project/feed/feed.js
+++ Project/feed/feed.js
@@ -2,7 +2,7 @@
  * Created by Carla on 4/6/2014.
  */
 
-var first_name = "Bob";
+var first_name = "Al";
 var last_name = "Jones";
 var a1 = "achieved Beginner Bunny";
 var a2 = "achieved Healthy Hare";
@@ -12,12 +12,17 @@ var b2 = "achieved Carrot Pro";
 var c1 = "achieved Slim Carrot";
 var c2 = "achieved Slender Carrot";
 var d1 = "achieved 5 Carrot Log";
+var d2 = "achieved 10 Carrot Log";
+var d3 = "achieved 15 Carrot Log";
 var profile_pic = ('<img src=male.jpg>');
 
 $("#add_to_feed").click(function () {
     $("ul").prepend('<li><div><table><tr>'+
             '<td><img src="male.jpg"></td>'+
-                '<td><h2 class ="adjust_indent">Bob Smith</h2><p class ="adjust_indent">achieved Beginner Bunny</p></td></tr>'+
+                '<td><h2 id="cur_name" class ="adjust_indent">'+first_name+" "+last_name +'</h2><p class ="adjust_indent">'+d2+'</p></td></tr>'+
         '</table></div></li>');
     $('ul').listview('refresh');
 });
+
+var element=document.getElementById("cur_name");
+element.innerHTML="New Header";
